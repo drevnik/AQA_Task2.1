34dbe7847fe835d235fc93368800746e04ba8390
Natalya Drevnik <natalyadrevnik@mail.ru>
2020-01-28 23:32:36 +0300

    Adding annotation

--- src/test/java/ru/netology/DebitCardApplication.java
+++ src/test/java/ru/netology/DebitCardApplication.java
@@ -1,16 +1,18 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DebitCardApplication {
 
     @Test
+    @DisplayName("Should submit request")
     void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $("[form form]");
