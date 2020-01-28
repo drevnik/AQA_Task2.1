702c8b50e591cbeeb52faa2ff2394796c11e32b9
Natalya Drevnik <natalyadrevnik@mail.ru>
2020-01-28 17:52:33 +0300

    Adding tests

---
+++ src/test/java/ru/netology/DebitCardApplication.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DebitCardApplication {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[form form]");
+
+        $("[data-test-id=name] input").setValue("Петр Петров");
+        $("[data-test-id=phone] input").setValue("+74785125462");
+        $("[data-test-id=agreement]").click();
+        $(By.tagName("button")).click();
+        $("[data-test-id=order-success]").should(exist);
+    }
+
+}
+
